2878a2f73be89f4d3f447a5b51f4a6ad55f3db1c
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-01-17 00:41:49 +0900

    [Init] Project 생성

---
+++ Machacha/MachachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachachaApp.swift
+//  Machacha
+//
+//  Created by geonhyeong on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct MachachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
