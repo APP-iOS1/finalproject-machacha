a54a807ecca670838b0679a3e6f27e34e95f8ee2
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-01-17 16:58:06 +0900

    [Feat] Model, AuthViewModel 폴더링

---
+++ Machacha/Sources/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Machacha
+//
+//  Created by geonhyeong on 2023/01/17.
+//
+
+import Foundation
---
+++ Machacha/Sources/ViewModels/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  Machacha
+//
+//  Created by geonhyeong on 2023/01/17.
+//
+
+import Foundation
