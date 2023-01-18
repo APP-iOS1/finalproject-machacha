1a56cc2d663816887f1b3175afeff10d4562bef0
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-01-18 16:22:42 +0900

    [Feat] App Login setting

--- Machacha/MachachaApp.swift
+++ Machacha/MachachaApp.swift
@@ -6,12 +6,60 @@
 //
 
 import SwiftUI
+import NaverThirdPartyLogin
+import KakaoSDKCommon
+import KakaoSDKAuth
+import GoogleSignIn
 
 @main
 struct MachachaApp: App {
+    init() {
+        // Naver SDK Initializing
+        
+        // 네이버 앱으로 로그인 허용
+        NaverThirdPartyLoginConnection.getSharedInstance()?.isNaverAppOauthEnable = true
+        // 브라우저 로그인 허용
+        NaverThirdPartyLoginConnection.getSharedInstance()?.isInAppOauthEnable = true
+        
+        // 네이버 로그인 세로모드 고정
+        NaverThirdPartyLoginConnection.getSharedInstance().setOnlyPortraitSupportInIphone(true)
+        
+        // NaverThirdPartyConstantsForApp.h에 선언한 상수 등록
+        NaverThirdPartyLoginConnection.getSharedInstance().serviceUrlScheme = kServiceAppUrlScheme
+        NaverThirdPartyLoginConnection.getSharedInstance().consumerKey = kConsumerKey
+        NaverThirdPartyLoginConnection.getSharedInstance().consumerSecret = kConsumerSecret
+        NaverThirdPartyLoginConnection.getSharedInstance().appName = kServiceAppName
+        
+        // Kakao SDK 초기화
+        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AuthView()
+                .onOpenURL { url in
+                    if NaverThirdPartyLoginConnection
+                        .getSharedInstance()
+                        //임의로 아무거나 넣어봄
+                        .isNaverAppOauthEnable
+                    //.isInAppOauthEnable
+                        //.isNaverThirdPartyLoginAppschemeURL(url)
+                    {
+                        // Token 발급 요청
+                        NaverThirdPartyLoginConnection
+                            .getSharedInstance()
+                            .receiveAccessToken(url)
+                    }
+                    
+                    
+                    GIDSignIn.sharedInstance.handle(url)
+                }
+                .onAppear {
+                          GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+                            // Check if `user` exists; otherwise, do something with `error`
+                          }
+                        }
         }
     }
 }
