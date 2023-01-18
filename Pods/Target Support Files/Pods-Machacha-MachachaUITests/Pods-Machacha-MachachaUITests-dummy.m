cefd4ccee933d227f6a43dd6b1f9c43cdaee780f
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-01-18 09:21:17 +0900

    [Add] naver map sdk 추가

---
+++ Pods/Target Support Files/Pods-Machacha-MachachaUITests/Pods-Machacha-MachachaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Machacha_MachachaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Machacha_MachachaUITests
+@end
---
+++ Pods/Target Support Files/Pods-Machacha/Pods-Machacha-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Machacha : NSObject
+@end
+@implementation PodsDummy_Pods_Machacha
+@end
