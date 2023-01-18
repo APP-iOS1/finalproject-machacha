964b8d96fd76e6fbe0a4f783d025abef09458284
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-01-18 17:13:32 +0900

    [Chore] AppDelegate 추가

--- Machacha/MachachaApp.swift
+++ Machacha/MachachaApp.swift
@@ -11,6 +11,52 @@ import KakaoSDKCommon
 import KakaoSDKAuth
 import GoogleSignIn
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        //FirebaseApp.configure()
+        // 원격 알림 등록
+        //UNUserNotificationCenter.current().delegate = self
+        
+//        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+//        UNUserNotificationCenter.current().requestAuthorization(
+//            options: authOptions,
+//            completionHandler: { _, _ in }
+//        )
+        
+        //application.registerForRemoteNotifications()
+        // 메세징 델리게이트
+        //Messaging.messaging().delegate = self
+        
+        // 푸시 포그라운드 설정
+        //UNUserNotificationCenter.current().delegate = self
+        
+        
+        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NAVTIVE_APP_KEY"] ?? ""
+        
+        //print("kakaoAppKey: \(kakaoAppKey)")
+        //Kakao SDK 초기화
+        KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
+        
+        return true
+    }
+    // fcm 토큰이 등록 되었을 때
+//    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//        Messaging.messaging().apnsToken = deviceToken
+//    }
+    // SceneDelegate  연결을 위한 함수
+//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+//
+//        let sceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+//
+//        sceneConfiguration.delegateClass = SceneDelegate.self
+//
+//        return sceneConfiguration
+//    }
+}
+
+
 @main
 struct MachachaApp: App {
     init() {
@@ -39,6 +85,8 @@ struct MachachaApp: App {
         WindowGroup {
             AuthView()
                 .onOpenURL { url in
+                    
+                    //네이버
                     if NaverThirdPartyLoginConnection
                         .getSharedInstance()
                         //임의로 아무거나 넣어봄
@@ -52,7 +100,12 @@ struct MachachaApp: App {
                             .receiveAccessToken(url)
                     }
                     
+                    //카카오
+                    if (AuthApi.isKakaoTalkLoginUrl(url)){
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
                     
+                    // 구글
                     GIDSignIn.sharedInstance.handle(url)
                 }
                 .onAppear {
