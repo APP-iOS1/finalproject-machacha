2ddc009571bdd80f9350ed0909c014287d184681
jeoneeee <wldus677@gmail.com>
2023-01-18 15:50:36 +0900

    [Feat] SplashView 완성

--- Machacha/Sources/Views/Splash/SplashView.swift
+++ Machacha/Sources/Views/Splash/SplashView.swift
@@ -8,14 +8,9 @@
 import SwiftUI
 
 struct SplashView: View {
-    func test() {
-        for family: String in UIFont.familyNames {
-            print(family)
-            for names : String in UIFont.fontNames(forFamilyName: family){
-                print("=== (names)")
-            }
-        }
-    }
+    @State private var isAnimation:Bool = false
+    @State private var rotation = 100.0
+    
     var body: some View {
         ZStack {
             
@@ -25,29 +20,28 @@ struct SplashView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: Screen.maxWidth * 1.2)
-                .offset(x: -32, y: 40 )
+                .offset(x: -32, y: rotation )
             
             
             Image("sweetpotato")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: Screen.maxWidth * 0.7)
-                .offset(x: -14, y: -10 )
+                .offset(x: -14, y: rotation - 50 )
             
             
             Image("fishcake")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: Screen.maxWidth * 0.66)
-                .offset(x: 90)
+                .offset(x: 90, y: rotation - 45)
             
             Image("bbungbread")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: Screen.maxWidth * 0.5)
                 .rotationEffect(Angle(degrees: 100))
-                .offset(x: 58, y: 15)
-            
+                .offset(x: 58, y: rotation - 10)
             
             Image("store")
                 .resizable()
@@ -57,23 +51,31 @@ struct SplashView: View {
                 .offset(y: -100)
             // 손글씨
             // 폰트
-            Text("마차챠")
-                .font(.custom("나눔손글씨 백의의 천사", size: 80))
-                .bold()
-                .offset(y: 100)
             
-            Button {
-                test()
-            } label: {
-                Text("Test")
-                    .font(.largeTitle)
+            if isAnimation {
+                
+                Text("마차챠")
+                    .font(.custom("나눔손글씨 느릿느릿체", size: 100))
+                    .bold()
+                    .offset(y: 100)
             }
-            .offset(y: 200)
             
             
             
         }
-        .listStyle(.automatic)
+        .background {
+            Color("cellColor")
+        }
+        .animation(.easeIn(duration: 1.5), value: rotation)
+        .onAppear {
+            rotation = 40
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                withAnimation {
+                    self.isAnimation = true
+                    rotation = 40
+                }
+            }
+        }
     }
 }
 
