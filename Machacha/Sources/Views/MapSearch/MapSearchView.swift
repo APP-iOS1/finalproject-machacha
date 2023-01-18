21d79f81f1ae9cfdca59991080e93f9e6e656aae
jwoo820 <pjwoo820@gmail.com>
2023-01-18 11:37:31 +0900

    [Feat] NaverMap Test code 추가
    맵 마커, 현재 위치, 확대, 축소 버튼 테스트 코드입니다.

--- Machacha/Sources/Views/MapSearch/MapSearchView.swift
+++ Machacha/Sources/Views/MapSearch/MapSearchView.swift
@@ -6,52 +6,35 @@
 //
 
 import SwiftUI
-import NMapsMap
 
 struct MapSearchView: View {
-    @State var coord: (Double, Double) = (126.986418, 37.560840)
     @StateObject var mapSerachViewModel = MapSearchViewModel()
     
     var body: some View {
         ZStack {
             VStack {
-                Button(action: {coord = (126.992227, 37.566249)}) {
+                Button {
+                    mapSerachViewModel.coord = (37.566249, 126.992227)
+                } label: {
                     Text("을지로3가역")
                 }
-                Button(action: {coord = (126.986418, 37.560840)}) {
+                
+                Button {
+                    mapSerachViewModel.coord = (37.560840, 126.986418)
+                } label: {
                     Text("명동역")
                 }
                 Spacer()
             }
             .zIndex(1)
             
-            UIMapView(coord: coord)
-                .edgesIgnoringSafeArea(.vertical)
+            NaverMap((mapSerachViewModel.coord.0, mapSerachViewModel.coord.1))
+                .ignoresSafeArea(.all, edges: .top)
         }
         .onAppear {
             mapSerachViewModel.checkIfLocationServicesIsEnabled()
         }
     }
-}
-
-struct UIMapView: UIViewRepresentable {
-    var coord: (Double, Double)
-  
-    func makeUIView(context: Context) -> NMFNaverMapView {
-        let view = NMFNaverMapView()
-        view.showZoomControls = false
-        view.mapView.positionMode = .direction
-        view.mapView.zoomLevel = 17
-      
-        return view
-    }
-    
-    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
-        let coord = NMGLatLng(lat: coord.1, lng: coord.0)
-        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
-        cameraUpdate.animation = .fly
-        cameraUpdate.animationDuration = 1
-        uiView.mapView.moveCamera(cameraUpdate)}
 }
 
 struct MapSearchView_Previews: PreviewProvider {
--- Machacha/Sources/Views/MapSearch/NaverMap.swift
+++ Machacha/Sources/Views/MapSearch/NaverMap.swift
@@ -5,4 +5,73 @@
 //  Created by Park Jungwoo on 2023/01/18.
 //
 
-import Foundation
+import SwiftUI
+import NMapsMap
+
+struct NaverMap: UIViewRepresentable {
+    var coord: (Double, Double)
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(coord)
+    }
+    
+    init(_ coord: (Double, Double)) {
+        self.coord = coord
+    }
+    
+    
+    class Coordinator: NSObject, NMFMapViewCameraDelegate {
+        var coord: (Double, Double)
+        init(_ coord: (Double, Double)) {
+            self.coord = coord
+        }
+        
+        
+        func mapView(_ mapView: NMFMapView, cameraWillChangeByReason reason: Int, animated: Bool) {
+            print("카메라 변경 - reason: \(reason)")
+        }
+        
+        func mapView(_ mapView: NMFMapView, cameraIsChangingByReason reason: Int) {
+            print("카메라 변경 - reason: \(reason)")
+        }
+    }
+    
+    func makeUIView(context: Context) -> some NMFNaverMapView {
+        let view = NMFNaverMapView()
+        view.showZoomControls = false
+        view.mapView.positionMode = .direction
+        view.mapView.zoomLevel = 17
+        let locationOverlay = view.mapView.locationOverlay
+        view.showLocationButton = true
+        view.showCompass = true
+        view.showZoomControls = true
+        let cameraPosition = view.mapView.cameraPosition
+        
+        let marker = NMFMarker()
+        marker.position = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
+        marker.iconImage = NMF_MARKER_IMAGE_BLACK
+        marker.iconTintColor = UIColor.green
+        marker.width = CGFloat(NMF_MARKER_SIZE_AUTO)
+        marker.height = CGFloat(NMF_MARKER_SIZE_AUTO)
+        
+        marker.captionRequestedWidth = 100
+        marker.captionText = "테스트 캡션"
+        marker.captionMinZoom = 12
+        marker.captionMaxZoom = 16
+        
+        marker.mapView = view.mapView
+        
+        print("camera pos: \(cameraPosition)")
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        let coord = NMGLatLng(lat: coord.0, lng: coord.1)
+        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
+        cameraUpdate.animation = .fly
+        cameraUpdate.animationDuration = 1
+        uiView.mapView.moveCamera(cameraUpdate)
+    }
+    
+}
