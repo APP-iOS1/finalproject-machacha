e986fbdbc12e790cdd0878954c3fe21ce4098454
jwoo820 <pjwoo820@gmail.com>
2023-01-24 16:03:22 +0900

    [Feat] Custom Marker를 위한 파일 생성

---
+++ src/frontend/user/Machacha/Sources/Views/MapSearch/CustomMarker.swift
@@ -0,0 +1,18 @@
+//
+//  CustomMarker.swift
+//  Machacha
+//
+//  Created by Park Jungwoo on 2023/01/24.
+//
+
+import Foundation
+import NMapsMap
+
+class CustomMarker {
+    
+    
+    init() {
+        let marker = NMFMarker()
+        
+    }
+}
