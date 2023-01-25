05580c94fa8085be87f0adcc452e2bbee2b5bbc9
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-01-25 19:46:16 +0900

    [Feat] UserDefaults를 통해 사용자 설정에 대한 값을 유지

--- src/frontend/user/Machacha/Sources/ViewModels/ProfileViewModel.swift
+++ src/frontend/user/Machacha/Sources/ViewModels/ProfileViewModel.swift
@@ -8,12 +8,31 @@
 import Foundation
 
 class ProfileViewModel: ObservableObject {
+	//MARK: Property wrapper
 	@Published var currentUser: User?
 	@Published var showLogin = false			// 로그인 관리
-	@Published var isFaceID = false				// FaceID
-	@Published var isAlert = false				// 알림
-	@Published var isDarkMode = false			// 다크모드
+	@Published var isFaceID: Bool = UserDefaults.standard.bool(forKey: "isFaceID") {		// FaceID
+		willSet { // 값이 변경되기 직전에 호출, newValue에는 새로 초기화하고자 하는 값이 들어감
+			UserDefaults.standard.set(newValue, forKey: "isFaceID")
+		}
+	}
+	@Published var isAlert: Bool = UserDefaults.standard.bool(forKey: "isAlert") {			// 알림
+		willSet {
+			UserDefaults.standard.set(newValue, forKey: "isAlert")
+		}
+	}
+	@Published var isDarkMode: Bool = UserDefaults.standard.bool(forKey: "isDarkMode") {	// 다크모드
+		willSet {
+			UserDefaults.standard.set(newValue, forKey: "isDarkMode")
+		}
+	}
 
+	init() { // 임시: 자동 로그인시 초기화 해줘야함
+		UserDefaults.standard.set(false, forKey: "isFaceID")	// FaceID
+		UserDefaults.standard.set(false, forKey: "isAlert")		// 알림
+		UserDefaults.standard.set(false, forKey: "isDarkMode")	// 다크모드
+	}
+	
 	// 로그아웃
 	func logout() async throws {
 		currentUser = nil
