2ad50683d7b243556dc5599ea08daaf52703feaf
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-01-25 16:40:21 +0900

    [Feat] 마차챠의 앱 정보를 List의 Section으로 분리하여 Web Site로 연결한 UI

--- src/frontend/user/Machacha/Sources/Views/Profile/ProfileView.swift
+++ src/frontend/user/Machacha/Sources/Views/Profile/ProfileView.swift
@@ -8,15 +8,78 @@
 import SwiftUI
 
 struct ProfileView: View {
+	//MARK: Property wrapper
 	@EnvironmentObject var userStateVM: ProfileViewModel
+	@State private var showSafari: Int?
+
+	//MARK: Property
+	let webInfo: [WebInfoType] = [.privacy, .openSource, .license]
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		List {
+			if let user = userStateVM.currentUser { // 사용자 정보
+				VStack(alignment: .leading) {
+					Text(user.name)
+						.font(.title3)
+				} // VStack
+				.padding([.top, .bottom], 10)
+			} else { // 로그인 요청 버튼
+				Button {
+					userStateVM.showLogin = true
+				} label: {
+					Text("로그인")
+						.fixedSize(horizontal: false, vertical: true)
+						.frame(maxWidth: .infinity, alignment: .center)
+				} // Button
+			} // if let user = userStateVM.currentUser
+			
+			WebViewInfo() // 마차챠에 대한 WebView Section
+			
+			if userStateVM.currentUser != nil {
+				Button(role: .destructive) {
+					Task {
+						try await userStateVM.logout()
+						userStateVM.showLogin = true
+					}
+				} label: {
+					HStack {
+						Spacer()
+						Text("로그아웃")
+						Spacer()
+					} // HStack
+				} // Button
+			} // userStateVM.currentUser != nil
+		} // List
+		.navigationTitle("프로필")
     }
+	
+	// 마차챠에 대한 WebView Section
+	@ViewBuilder
+	private func WebViewInfo() -> some View {
+		Section {
+			ForEach(Array(webInfo.enumerated()), id: \.offset) { i, web in
+				Button {
+					self.showSafari = i // 선택된 Web Safari 정보
+				} label: {
+					Text("\(web.display)")
+						.fixedSize(horizontal: false, vertical: true)
+						.frame(maxWidth: .infinity, alignment: .leading)
+				} // Button
+			} // ForEach
+		} header: {
+			Text("Machacha 정보")
+		} // 마차챠에 대한 WebView Section
+		.sheet(item: $showSafari) {
+			SafariView(url: URL(string: webInfo[$0].url)!)
+		}
+	}
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileView()
+		NavigationView {
+			ProfileView()
+				.environmentObject(ProfileViewModel())
+		}
     }
 }
