7d5017e6787b3678afcd2bd193dbded72addc14f
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-01-31 00:44:10 +0900

    [Feat] 스캘레톤View의 색상 및 데이터가 나타나면 opacity 조절

--- src/frontend/user/Machacha/Configuration/Modifiers/AnimationModifier.swift
+++ src/frontend/user/Machacha/Configuration/Modifiers/AnimationModifier.swift
@@ -10,6 +10,7 @@ import SwiftUI
 //MARK: 반짝이는 스켈레톤 View
 struct BlinkingAnimatinoModifier: AnimatableModifier {
 	//MARK: Property
+	var shouldShow: Bool
 	var opacity: Double
 	var animatableData: Double {
 		get { opacity }
@@ -22,15 +23,15 @@ struct BlinkingAnimatinoModifier: AnimatableModifier {
 			ZStack {
 				Color.white.zIndex(0)					// 뒤의 Conent가 보이지 않도록
 				RoundedRectangle(cornerRadius: 8)
-					.fill(.gray)
+					.fill(.gray.opacity(0.5))
 					.opacity(self.opacity).zIndex(1)	// 우선순위 1
-			}
+			}.opacity(shouldShow ? 1 : 0)
 		)
 	}
 }
 
 extension View {
-	func setSkeletonView(opacity: Double) -> some View {
-		self.modifier(BlinkingAnimatinoModifier(opacity: opacity))
+	func setSkeletonView(opacity: Double, shouldShow: Bool) -> some View {
+		self.modifier(BlinkingAnimatinoModifier(shouldShow: shouldShow, opacity: opacity))
 	}
 }
