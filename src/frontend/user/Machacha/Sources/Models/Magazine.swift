6d2b7ac7eebca9045a88fbc62b47512f11f45a0f
sue <suekim999@naver.com>
2023-02-01 14:21:46 +0900

    [Add] Magazine 모델 내부 변경

--- src/frontend/user/Machacha/Sources/Models/Magazine.swift
+++ src/frontend/user/Machacha/Sources/Models/Magazine.swift
@@ -9,16 +9,20 @@ import Foundation
 
 struct Magazine: Identifiable {
 	let id: String
-	let foodCartId: [String]        // foodCart들의 id
-	let pickTitle: String           // ~~~'s PICK
-	let title: String               // Main Title
-	let subTitle: String            // SubTitle
-	let comment: String             // 큐레이터의 한마디
-	let thumbnail: String           // 매거진의 Main Image, storageId
-	let updatedAt: Date
-	let createdAt: Date
+    let thumbTitle: String               // Main Title
+    let thumbnail: String           // 매거진의 Main Image, storageId
+    let editorPickTitle: String           // ~~~'s PICK
+    let subTitle: String            // SubTitle
+    let comment: String             // 큐레이터의 한마디
+    let foodCartId: [String]        // foodCart들의 id
+    let updatedAt: Date
+    let createdAt: Date
+    var show: Bool //새로 추가
+    
+    static func getDummy() -> Self {
+        return Magazine(id: "ECch0WHCMyJN6bqzranq", thumbTitle: "붕어빵", thumbnail: "p3",  editorPickTitle: "겨울 조하's PICK", subTitle: "명동의 붕어빵을 찾아 떠난 지방러", comment: "지방러들을 위해 명동의 간식 포장마차를 대신 다녀왔습니다. 행복했던 시간을 기록해 봅니다.", foodCartId: ["InzqNwgl15TytWNOdIZz"], updatedAt: Date(), createdAt: Date(), show: false)
+    }
 	
-	static func getDummy() -> Self {
-		return Magazine(id: "9HYoFd0JG8ZPqVbDNSwE", foodCartId: ["InzqNwgl15TytWNOdIZz"], pickTitle: "겨울이 좋은 마챠차's PICK", title: "만원으로 명동을 돌아보기", subTitle: "명동의 간식 포장마차를 찾아 떠난 지방러", comment: "지방러들을 위해 명동의 간식 포장마차를 대신 다녀왔습니다. 행복했던 시간을 기록해 봅니다.", thumbnail: "test", updatedAt: Date(), createdAt: Date())
-	}
+
 }
+
--- src/frontend/user/Machacha/Sources/Views/Magazine/MagazineView.swift
+++ src/frontend/user/Machacha/Sources/Views/Magazine/MagazineView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct MagazineView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text("hello")
+        }
     }
 }
 
