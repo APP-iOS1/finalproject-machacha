060270d1f92ab54f788f32507fa4efd986b8a703
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-02-01 09:49:50 +0900

    [Feat] AuthView userCheck switch문 추가

---
+++ src/frontend/user/Machacha/Sources/Views/Login/AuthView.swift
@@ -0,0 +1,55 @@
+//
+//  AuthView.swift
+//  Machacha
+//
+//  Created by MacBook on 2023/01/31.
+//
+
+import Foundation
+import SwiftUI
+import NaverThirdPartyLogin
+import GoogleSignInSwift
+import GoogleSignIn
+import _AuthenticationServices_SwiftUI
+
+enum LoginState {
+    case authenticated
+    case unauthenticated
+    case authenticating
+    case pass
+}
+
+struct AuthView : View {
+    //@State var loginState : LoginState = .unauthenticated
+    @StateObject var authVM = AuthViewModel()//.shared
+    @StateObject var userVM = UserViewModel.shared
+    
+    var body: some View {
+        switch authVM.loginState {
+        case .unauthenticated, .none :
+            LoginView()
+                .environmentObject(authVM)
+        case .authenticating :
+            ProgressView()
+        case .authenticated :
+            switch userVM.userCheck {
+            case .checking :
+                ProgressView()
+            case .incorrect :
+                LoginView()
+                    .environmentObject(authVM)
+            case .correct :
+                ContentView()
+            case .firstLogin :
+                SignUpView()
+                    .environmentObject(authVM)
+                    .environmentObject(userVM)
+            }
+        case .pass:
+            ContentView()
+                .onAppear{
+                    print(authVM.loginState!)
+                }
+        }
+    }
+}
