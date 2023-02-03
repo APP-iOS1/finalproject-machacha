59f959f2eba1bfb7b828c5b28bfcc1393ba43648
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-02-03 22:05:31 +0900

    [Feat] ProfileEditView UI

---
+++ src/frontend/user/Machacha/Sources/Views/Profile/ProfileEditView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileEditView.swift
+//  Machacha
+//
+//  Created by geonhyeong on 2023/02/03.
+//
+
+import SwiftUI
+
+struct ProfileEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditView()
+    }
+}
