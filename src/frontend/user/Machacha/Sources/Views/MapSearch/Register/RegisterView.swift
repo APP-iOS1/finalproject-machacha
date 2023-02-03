0fc17367d27375f4a38aa9464333ebe1a848f210
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-02-03 15:41:17 +0900

    [Feat] RegisterView

--- src/frontend/user/Machacha/Sources/Views/MapSearch/Register/RegisterView.swift
+++ src/frontend/user/Machacha/Sources/Views/MapSearch/Register/RegisterView.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @ObservedObject var mapSerachViewModel = MapSearchViewModel() //현재 위치
+    @State var cameraCoord : (Double,Double) = (37.566249, 126.992227) // 카메라 위치
+    @State var coordinate: (Double, Double) = (37.566249, 126.992227)
+    @State var isTap: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                Circle()
+                    .foregroundColor(Color("Color3"))
+                    .frame(width: 20)
+                    .zIndex(1)
+                RegisterNaverMap(coord: $mapSerachViewModel.coord, cameraCoord: $cameraCoord)
+                    .ignoresSafeArea(.all, edges: .top)
+            }
+            .onAppear {
+                mapSerachViewModel.checkIfLocationServicesIsEnabled()
+                coordinate = mapSerachViewModel.coord
+            }
+            .onChange(of: cameraCoord.0) { coord in
+                print("변화중 : \(cameraCoord)")
+            }
+            
+            Text("현재 위치: \(cameraCoord.0), \(cameraCoord.1)")
+            NavigationLink(destination: EmptyView()) {
+                Text("등록")
+            }
+        }
     }
 }
 
 struct RegisterView_Previews: PreviewProvider {
     static var previews: some View {
         RegisterView()
+    }
+}
+
+
+struct Coord : Equatable {
+    var latitude : Double
+    var longitude : Double
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    static func ==(left: Coord, right: Coord) -> Bool {          // Needed to be Equatable
+        return left.latitude == right.latitude && left.longitude == right.longitude
     }
 }
