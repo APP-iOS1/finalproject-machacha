2f821b9c5634e7466ec4f9d1742377a46628dc76
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-02-05 10:04:22 +0900

    [Feat] RegisterNaverMap 카메라중앙마커 기능

--- src/frontend/user/Machacha/Sources/Views/MapSearch/Register/RegisterNaverMap.swift
+++ src/frontend/user/Machacha/Sources/Views/MapSearch/Register/RegisterNaverMap.swift
@@ -7,8 +7,9 @@ import NMapsMap
 struct RegisterNaverMap: UIViewRepresentable {
     @Binding var coord: (Double, Double)
     @Binding var cameraCoord : (Double,Double)
+    @State var markers : [NMFMarker] = [NMFMarker(position: NMGLatLng(lat: 37.566249, lng: 126.992227))]
     func makeCoordinator() -> NMCoordinator {
-        NMCoordinator(coord,cameraCoord)
+        NMCoordinator(coord,$cameraCoord,$markers)
     }
 
     // MARK: - Map을 그리고 생성하는 메서드
@@ -17,29 +18,36 @@ struct RegisterNaverMap: UIViewRepresentable {
         view.showZoomControls = false
         view.mapView.positionMode = .direction
         view.mapView.zoomLevel = 17
-//        view.showLocationButton = true
-//
+        view.showLocationButton = true
+        
         view.mapView.addCameraDelegate(delegate: context.coordinator)
         view.mapView.touchDelegate = (context.coordinator)
+        let cameraCoord = NMGLatLng(lat: coord.0, lng: coord.1)
+        let cameraUpdate = NMFCameraUpdate(scrollTo: cameraCoord)
+        view.mapView.moveCamera(cameraUpdate)
+        //let a = view.mapView.projection.point(from: NMGLatLng(lat: coord.0, lng: coord.1))
         return view
     }
     
     // MARK: - Map이 업데이트 될 때 발생하는 메서드
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        let cameraCoord = NMGLatLng(lat: cameraCoord.0, lng: cameraCoord.1)
-        let cameraUpdate = NMFCameraUpdate(scrollTo: cameraCoord)
+        let coord = NMGLatLng(lat: cameraCoord.0, lng: cameraCoord.1)
+        let cameraUpdate = NMFCameraUpdate(scrollTo: coord)
         cameraUpdate.animation = .easeIn
         cameraUpdate.animationDuration = 0.3
         uiView.mapView.moveCamera(cameraUpdate)
-        print("update: \(cameraCoord)")
+        print("update: \(coord)")
     }
     
     class NMCoordinator: NSObject ,NMFMapViewCameraDelegate,NMFMapViewTouchDelegate  {
         var coord: (Double, Double)
-        var cameraCoord : (Double, Double)
-        init(_ coord: (Double, Double), _ cameraCoord: (Double, Double)) {
+        @Binding var cameraCoord : (Double, Double)
+        @Binding var markers : [NMFMarker]
+        
+        init(_ coord: (Double, Double), _ cameraCoord: Binding<(Double, Double)>, _ markers : Binding<[NMFMarker]>) {
             self.coord = coord
-            self.cameraCoord = cameraCoord
+            self._cameraCoord = cameraCoord
+            self._markers = markers
         }
         
         // MARK: - 카메라 이동시 발생하는 Delegate
@@ -55,10 +63,24 @@ struct RegisterNaverMap: UIViewRepresentable {
 
         // 카메라의 움직임이 끝났을 때 호출
         func mapView(_ mapView: NMFMapView, cameraDidChangeByReason reason: Int, animated: Bool) {
-            //coord = (mapView.cameraPosition.target.lat, mapView.cameraPosition.target.lng)
-            cameraCoord = (mapView.cameraPosition.target.lat, mapView.cameraPosition.target.lng)
-            print("현재 카메라 좌표 : \(cameraCoord)")
+            let point = (mapView.cameraPosition.target.lat, mapView.cameraPosition.target.lng)
+            if cameraCoord != point {
+                cameraCoord = (mapView.cameraPosition.target.lat, mapView.cameraPosition.target.lng)
+                let marker = NMFMarker()
+                marker.position = NMGLatLng(lat: cameraCoord.0, lng: cameraCoord.1)
+                marker.isHideCollidedMarkers = true
+                marker.mapView = mapView
+                markers.append(marker)
+                if !markers.isEmpty {
+                    let removeMarker = markers.removeFirst()
+                    removeMarker.mapView = nil
+                }
+                print("중앙 마커 이동")
+            }
+            print("현재 카메라 좌표 : \(coord)")
         }
+        
+        
         
         // MARK: - 지도 터치에 이용되는 Delegate
         func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
@@ -68,36 +90,3 @@ struct RegisterNaverMap: UIViewRepresentable {
     }
     
 }
-
-//class NMCoordinator: NSObject {
-//    var coord: (Double, Double)
-//    init(_ coord: (Double, Double)) {
-//        self.coord = coord
-//    }
-//}
-//
-//// MARK: - 카메라 이동시 발생하는 Delegate
-//extension NMCoordinator: NMFMapViewCameraDelegate {
-//    // 카메라의 움직임이 시작할 때 호출
-//    func mapView(_ mapView: NMFMapView, cameraWillChangeByReason reason: Int, animated: Bool) {
-////        print("카메라 변경 - reason: \(reason)")
-//    }
-//
-//    // 카메라가 움직이고 있을 때 호출
-//    func mapView(_ mapView: NMFMapView, cameraIsChangingByReason reason: Int) {
-//
-//    }
-//
-//    // 카메라의 움직임이 끝났을 때 호출
-//    func mapView(_ mapView: NMFMapView, cameraDidChangeByReason reason: Int, animated: Bool) {
-//        coord = (mapView.cameraPosition.target.lat, mapView.cameraPosition.target.lng)
-//        print("현재 카메라 좌표 : \(coord)")
-//    }
-//}
-//
-//// MARK: - 지도 터치에 이용되는 Delegate
-//extension NMCoordinator: NMFMapViewTouchDelegate {
-//    func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
-//        print("Map Tapped")
-//    }
-//}
