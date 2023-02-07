1f8ff3bfc224817038115d8052c52364aeeb1417
jeoneeee <wldus677@gmail.com>
2023-02-07 22:07:43 +0900

    [Feat] 리뷰 삭제 시 Alert, AlertToast 기능 추가

--- src/frontend/user/Machacha/Sources/Views/Detail/ReviewView.swift
+++ src/frontend/user/Machacha/Sources/Views/Detail/ReviewView.swift
@@ -14,49 +14,49 @@ struct ReviewView: View {
     var selectedStore: FoodCart
     
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack(alignment: .center) {
-                Image(systemName: "circle.fill")
-                    .resizable()
-                    .frame(width: 5, height: 5)
-                    .foregroundColor(Color("Color3"))
-                    .padding(.leading, 10)
+            VStack(alignment: .leading) {
+                HStack(alignment: .center) {
+                    Image(systemName: "circle.fill")
+                        .resizable()
+                        .frame(width: 5, height: 5)
+                        .foregroundColor(Color("Color3"))
+                        .padding(.leading, 10)
                     
-                Text("최신순")
-                    .font(.machachaHeadline)
-                    .padding(.bottom, 5)
-            }
-            .padding(.top, 15)
-            ScrollView {
-                ForEach(reviewViewModel.reviews, id: \.self) { review in
-                    Divider()
-                        .padding(.vertical)
-                    ReviewListCellView(review: review)
-                        .frame(width: 350, alignment: .leading)
-                        .alert(isPresented: $reviewViewModel.isShowingAlert) {
-                            // 허용안함, FaceID error
-                            Alert(
-                                title: Text("삭제하시겠습니까?"),
-                                primaryButton: .default(Text("취소")) {
-                                },
-                                secondaryButton: .default(Text("삭제")) {
-//                                    reviewViewModel.removeDiary(review: review)
-                                    Task {
-                                        await reviewViewModel.fetchReviews(foodCartId: selectedStore.id)
-                                    }
-                                    showToast = true
-                                })
-                        }
+                    Text("최신순")
+                        .font(.machachaHeadline)
+                        .padding(.bottom, 5)
                 }
-            }//ScrollView
-        }//VStack
-        .toast(isPresenting: $showToast){
-            
-            AlertToast(displayMode: .banner(.pop), type: .regular, title: "리뷰가 삭제되었습니다.")
-            
-            //Choose .hud to toast alert from the top of the screen
-            //AlertToast(displayMode: .hud, type: .regular, title: "Message Sent!")
-        }
+                .padding(.top, 15)
+                ScrollView {
+                    ForEach(reviewViewModel.reviews, id: \.self) { review in
+                        Divider()
+                            .padding(.vertical)
+                        ReviewListCellView(review: review)
+                            .frame(width: 350, alignment: .leading)
+                            .alert(isPresented: $reviewViewModel.isShowingAlert) {
+                                //댓글 삭제 시 알럿
+                                Alert(
+                                    title: Text("삭제하시겠습니까?"),
+                                    primaryButton: .default(Text("취소")) {
+                                    },
+                                    secondaryButton: .default(Text("삭제")) {
+                                        //                                    reviewViewModel.removeDiary(review: review)
+                                        Task {
+                                            await reviewViewModel.fetchReviews(foodCartId: selectedStore.id)
+                                        }
+                                        showToast = true
+                                    })
+                            }
+                    }
+                }//ScrollView
+            }//VStack
+            .toast(isPresenting: $showToast){
+                //댓글 삭제 후 화면 하단 토스트 메세지 출력
+                AlertToast(displayMode: .banner(.pop), type: .regular, title: "리뷰가 삭제되었습니다.")
+            }
+            .fullScreenCover(isPresented: $reviewViewModel.isShowingReportSheet) {
+                ReportView(reportType: 2)
+            }
 //        .onAppear {
 //             reviewViewModel.isLoading = true
 //            Task {
