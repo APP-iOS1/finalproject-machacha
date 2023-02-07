736566ffd90e11e01674cc8eb52718153f5a3d26
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-02-08 00:06:03 +0900

    [Init] Admin Machacha Project

---
+++ src/frontend/admin/MachachaAdmin/MachachaAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachachaAdminApp.swift
+//  MachachaAdmin
+//
+//  Created by geonhyeong on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct MachachaAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
