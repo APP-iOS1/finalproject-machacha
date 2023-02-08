c1d6026e116bc19df5f065dd2cb128a8aa75abf1
jwoo820 <pjwoo820@gmail.com>
2023-02-08 10:43:38 +0900

    [Chore] NaverMap Coordinator 수정

--- src/frontend/user/Machacha/Sources/Views/MapSearch/NaverMap.swift
+++ src/frontend/user/Machacha/Sources/Views/MapSearch/NaverMap.swift
@@ -83,7 +83,6 @@ struct NaverMap: UIViewRepresentable {
             cameraUpdate.animation = .easeIn
             cameraUpdate.animationDuration = 0.3
             uiView.mapView.moveCamera(cameraUpdate)
-    //        mapSearchViewModel.zoomLevel = uiView.mapView.zoomLevel
             print("currentindex : \(currentIndex)")
             // 이부분에서 Marker가 커지는 작업을 처리해줘야함 -> 마커 생성을 밖에서 해줘야할 거 같음
             for (index, marker) in markers.enumerated() {
@@ -108,43 +107,23 @@ struct NaverMap: UIViewRepresentable {
         }
     
     }
-}
-
-class Coordinator: NSObject {
-    @Binding var cameraPosition: LatLng
-    @Binding var zoomLevel: Double
-    
-    init(_ cameraPosition: Binding<(LatLng)>, _ zoomLevel: Binding<Double>) {
-        _cameraPosition = cameraPosition
-        _zoomLevel = zoomLevel
-    }
-}
-
-// MARK: - 카메라 이동시 발생하는 Delegate
-extension Coordinator: NMFMapViewCameraDelegate {
-    // 카메라의 움직임이 시작할 때 호출
-    func mapView(_ mapView: NMFMapView, cameraWillChangeByReason reason: Int, animated: Bool) {
-        //        print("카메라 변경 - reason: \(reason)")
-    }
-    
-    // 카메라가 움직이고 있을 때 호출
-    func mapView(_ mapView: NMFMapView, cameraIsChangingByReason reason: Int) {
+    class Coordinator: NSObject, NMFMapViewCameraDelegate, NMFMapViewTouchDelegate {
+        @Binding var cameraPosition: LatLng
+        @Binding var zoomLevel: Double
         
+        init(_ cameraPosition: Binding<(LatLng)>, _ zoomLevel: Binding<Double>) {
+            _cameraPosition = cameraPosition
+            _zoomLevel = zoomLevel
+        }
+        // MARK: - 카메라 이동이 끝났음을 알려주는 이벤트
+        func mapViewCameraIdle(_ mapView: NMFMapView) {
+            cameraPosition = (mapView.cameraPosition.target.lat, mapView.cameraPosition.target.lng)
+            zoomLevel = mapView.cameraPosition.zoom
+        }
+        // MARK: - 맵 터치 이벤트
+        func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
+            print("Map Tapped")
+        }
     }
     
-    // 카메라의 움직임이 끝났을 때 호출
-    func mapView(_ mapView: NMFMapView, cameraDidChangeByReason reason: Int, animated: Bool) {
-
-    }
-    func mapViewCameraIdle(_ mapView: NMFMapView) {
-        cameraPosition = (mapView.cameraPosition.target.lat, mapView.cameraPosition.target.lng)
-        zoomLevel = mapView.cameraPosition.zoom
-    }
-}
-
-// MARK: - 지도 터치에 이용되는 Delegate
-extension Coordinator: NMFMapViewTouchDelegate {
-    func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
-        print("Map Tapped")
-    }
 }
