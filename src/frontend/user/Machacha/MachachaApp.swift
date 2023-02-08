a42d120085437d3d84eb9a3631932f00c9645abc
jwoo820 <pjwoo820@gmail.com>
2023-02-08 23:44:30 +0900

    [Feat] FCM 기능
    프로젝트 세팅은 미완

--- src/frontend/user/Machacha/MachachaApp.swift
+++ src/frontend/user/Machacha/MachachaApp.swift
@@ -11,29 +11,30 @@ import KakaoSDKCommon
 import KakaoSDKAuth
 import GoogleSignIn
 import Firebase
+import FirebaseMessaging
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        // 원격 알림 등록
-        //UNUserNotificationCenter.current().delegate = self
-        
-        //        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        //        UNUserNotificationCenter.current().requestAuthorization(
-        //            options: authOptions,
-        //            completionHandler: { _, _ in }
-        //        )
-        
-        //application.registerForRemoteNotifications()
-        // 메세징 델리게이트
-        //Messaging.messaging().delegate = self
-        
-        // 푸시 포그라운드 설정
-        //UNUserNotificationCenter.current().delegate = self
-        
         //파이어베이스 초기화
         FirebaseApp.configure()
+        
+        // 원격 알림 등록
+        UNUserNotificationCenter.current().delegate = self
+        
+                let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+                UNUserNotificationCenter.current().requestAuthorization(
+                    options: authOptions,
+                    completionHandler: { _, _ in }
+                )
+        
+        application.registerForRemoteNotifications()
+        // 메세징 델리게이트
+        Messaging.messaging().delegate = self
+        
+        // 푸시 포그라운드 설정
+        UNUserNotificationCenter.current().delegate = self
         
         return true
     }
@@ -44,18 +45,51 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     //    }
     
     // fcm 토큰이 등록 되었을 때
-    //    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    //        Messaging.messaging().apnsToken = deviceToken
-    //    }
+        func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+            Messaging.messaging().apnsToken = deviceToken
+        }
     // SceneDelegate  연결을 위한 함수
-    //    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    //
-    //        let sceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
-    //
-    //        sceneConfiguration.delegateClass = SceneDelegate.self
-    //
-    //        return sceneConfiguration
-    //    }
+        func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    
+            let sceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+    
+            sceneConfiguration.delegateClass = SceneDelegate.self
+    
+            return sceneConfiguration
+        }
+}
+
+extension AppDelegate: MessagingDelegate {
+    // fcm 등록 토큰을 받았을 때
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("AppDelegate - 파베 토큰 받음")
+        print("AppDelegate - Firebase registration token: \(String(describing: fcmToken))")
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // 푸시메세지가 앱이 켜져 있을 때 나옴
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        
+        completionHandler([.banner, .sound, .badge])
+    }
+    
+    // 백그라운드에서 푸시메세지를 받았을 때
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        print("didReceive: userInfo: ", userInfo)
+        completionHandler()
+    }
+}
+
+// MARK: - 앱 실행시 앱 아이콘의 뱃지 알림 초기화
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        if UIApplication.shared.applicationIconBadgeNumber != 0 {
+            UIApplication.shared.applicationIconBadgeNumber = 0
+        }
+    }
 }
 
 @main
