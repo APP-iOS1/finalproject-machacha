2ac5417e52a25288e7c6d458bd143da5e3c3ffff
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-02-08 13:49:12 +0900

    [Feat] Tab icon

--- src/frontend/admin/MachachaAdmin/Sources/Views/ContentView.swift
+++ src/frontend/admin/MachachaAdmin/Sources/Views/ContentView.swift
@@ -13,45 +13,41 @@ struct ContentView: View {
 
 	//MARK: Property
 	
-    var body: some View {
+	var body: some View {
 		TabView(selection: $selection) {
 			Text("첫번째 뷰")
-				.font(.system(size: 30))
 				.tabItem {
-					Image(systemName: "house.fill")
+					Image(systemName: "plus.circle.fill")
 					Text("승인")
 				}.tag(0)
 			
 			Text("두번째 뷰")
-				.font(.system(size: 30))
 				.tabItem {
 					Image(systemName: "light.beacon.max.fill")
 					Text("신고")
 				}.tag(1)
 			
 			Text("세번째 뷰")
-				.font(.system(size: 30))
 				.tabItem {
-					Image(systemName: "video.circle.fill")
+					Image(systemName: "newspaper.fill")
 					Text("매거진")
 				}.tag(2)
 			
 			Text("네번째 뷰")
-				.font(.system(size: 30))
 				.tabItem {
-					Image(systemName: "video.circle.fill")
+					Image(systemName: "bell.fill")
 					Text("공지알림")
 				}.tag(3)
 			
 			Text("다섯번째 뷰")
-				.font(.system(size: 30))
 				.tabItem {
-					Image(systemName: "video.circle.fill")
+					Image(systemName: "person.fill")
 					Text("프로필")
 				}.tag(4)
 		} // TabView
 	}
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
