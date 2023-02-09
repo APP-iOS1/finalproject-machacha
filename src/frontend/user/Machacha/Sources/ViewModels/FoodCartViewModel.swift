604de4780bef2cfba9b6a80aef6d78e85318eb4d
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-02-09 16:39:14 +0900

    [Feat] 리뷰관리 Cell UI

--- src/frontend/user/Machacha/Sources/ViewModels/FoodCartViewModel.swift
+++ src/frontend/user/Machacha/Sources/ViewModels/FoodCartViewModel.swift
@@ -92,7 +92,7 @@ class FoodCartViewModel: ObservableObject {
                 let reportCnt: Int = data["reportCnt"] as? Int ?? 0
                 let menu: [String: Int] = data["menu"] as? [String: Int] ?? [:]
                 let bestMenu: Int = data["bestMenu"] as? Int ?? 0
-                let paymentOpt: [Bool] = data["isPaymentOpt"] as? [Bool] ?? []
+                let paymentOpt: [Bool] = data["paymentOpt"] as? [Bool] ?? []
                 let openingDays: [Bool] = data["openingDays"] as? [Bool] ?? []
                 let reviewId: [String] = data["reviewId"] as? [String] ?? []
                 let updatedAt: Timestamp = data["updatedAt"] as! Timestamp
@@ -112,6 +112,42 @@ class FoodCartViewModel: ObservableObject {
             print("fetchFoodCarts error: \(error.localizedDescription)")
         }
     }
+	
+	// MARK: - 서버에서 FoodCart Collection의 데이터들을 불러오는 Method
+	func fetchFoodCartByFoodCartId(_ foodcartId: String) async -> FoodCart {
+		var foodCart = FoodCart.getDummy()
+		
+		do {
+			let querySnapshot = try await database.collection("FoodCart")
+				.document(foodcartId).getDocument()
+			
+			let data = querySnapshot.data()!
+			
+			let id = data["id"] as? String ?? ""
+			let name: String = data["name"] as? String ?? ""
+			let region: String = data["region"] as? String ?? ""
+			let address: String = data["address"] as? String ?? ""
+			let geoPoint: GeoPoint = data["geoPoint"] as! GeoPoint
+			let imageId: [String] = data["imageId"] as? [String] ?? []
+			let grade: Double = data["grade"] as? Double ?? 0
+			let visitedCnt: Int = data["visitedCnt"] as? Int ?? 0
+			let favoriteCnt: Int = data["favoriteCnt"] as? Int ?? 0
+			let reportCnt: Int = data["reportCnt"] as? Int ?? 0
+			let menu: [String: Int] = data["menu"] as? [String: Int] ?? [:]
+			let bestMenu: Int = data["bestMenu"] as? Int ?? 0
+			let paymentOpt: [Bool] = data["paymentOpt"] as? [Bool] ?? []
+			let openingDays: [Bool] = data["openingDays"] as? [Bool] ?? []
+			let reviewId: [String] = data["reviewId"] as? [String] ?? []
+			let updatedAt: Timestamp = data["updatedAt"] as! Timestamp
+			let createdAt: Timestamp = data["createdAt"] as! Timestamp
+			let registerId: String = data["registerId"] as? String ?? ""
+			
+			foodCart = FoodCart(id: id, createdAt: createdAt.dateValue(), updatedAt: updatedAt.dateValue(), geoPoint: geoPoint, region: region, name: name, address: address, visitedCnt: visitedCnt, favoriteCnt: favoriteCnt, paymentOpt: paymentOpt, openingDays: openingDays, menu: menu, bestMenu: bestMenu, imageId: imageId, grade: grade, reportCnt: reportCnt, reviewId: reviewId, registerId: registerId)
+		} catch {
+			print("fetchFoodCarts error: \(error.localizedDescription)")
+		}
+		return foodCart
+	}
     
     // MARK: - 서버의 Storage에서 이미지를 가져오는 Method
     func fetchImage(foodCartId: String, imageName: String) {
--- src/frontend/user/Machacha/Sources/Views/Profile/ProfileFoodCartListView.swift
+++ src/frontend/user/Machacha/Sources/Views/Profile/ProfileFoodCartListView.swift
@@ -11,6 +11,7 @@ struct ProfileFoodCartListView: View {
 	//MARK: Property wrapper
 	@Environment(\.presentationMode) var presentation
 	@EnvironmentObject var profileVM: ProfileViewModel
+	@State private var isLoading = false // 리뷰관리 loading
 	
 	//MARK: Property
 	let foodCartOfUserType: FoodCartOfUserType
@@ -19,9 +20,15 @@ struct ProfileFoodCartListView: View {
 		ScrollView {
 			Section {
 				LazyVStack(alignment: .leading, spacing: 15) {
-					ForEach(profileVM.foodCartUser) { foodCart in
-						ProfileFoodCartCellView(foodCartOfUserType: foodCartOfUserType, foodCart: foodCart, isFavorite: profileVM.currentUser!.favoriteId.contains(foodCart.id))
-					} // ForEach
+					if foodCartOfUserType == .review {
+						ForEach(profileVM.reviewUser) { review in
+							ProfileFoodCartReviewCellView(isLoading: $isLoading, review: review)
+						} // ForEach
+					} else {
+						ForEach(profileVM.foodCartUser) { foodCart in
+							ProfileFoodCartCellView(foodCartOfUserType: foodCartOfUserType, foodCart: foodCart, isFavorite: profileVM.currentUser!.favoriteId.contains(foodCart.id))
+						} // ForEach
+					}
 				} // VStack
 			} header: {
 				HStack {
@@ -34,15 +41,21 @@ struct ProfileFoodCartListView: View {
 			} // Section
 		} // ScrollView
 		.refreshable(action: {
-			profileVM.isLoading = true
-			Task {
-				profileVM.foodCartUser = try await profileVM.fetchFoodCartByType(foodCartType: foodCartOfUserType)
+			if foodCartOfUserType == .review {
+				isLoading = true
 				DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) { // 스켈레톤 View를 위해
-					profileVM.isLoading = false
+					isLoading = false
 				} // DispatchQueue
-			} // Task
+			} else {
+				profileVM.isLoading = true
+				Task {
+					profileVM.foodCartUser = try await profileVM.fetchFoodCartByType(foodCartType: foodCartOfUserType)
+					DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) { // 스켈레톤 View를 위해
+						profileVM.isLoading = false
+					} // DispatchQueue
+				} // Task
+			}
 		})
-		.redacted(reason: profileVM.isLoading ? .placeholder : [])	// 콘텐츠 모자이크
 		.navigationBarBackButtonHidden()
 		.navigationBarTitle("\(foodCartOfUserType.display)", displayMode: .large)
 		.toolbarBackground(Color("Color3"), for: .navigationBar)
@@ -53,7 +66,10 @@ struct ProfileFoodCartListView: View {
 				Button {
 					self.presentation.wrappedValue.dismiss()
 				} label: {
-					Image(systemName: "chevron.left")
+					HStack {
+						Image(systemName: "chevron.left")
+						Text("프로필")
+					}
 				}
 			} // ToolbarItem
 		})
@@ -62,9 +78,7 @@ struct ProfileFoodCartListView: View {
 			profileVM.isLoading = true
 			Task {
 				profileVM.foodCartUser = try await profileVM.fetchFoodCartByType(foodCartType: foodCartOfUserType) // 타입별로 데이터를 비동기로 불러오는 값이 다르게 처리
-				DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) { // 스켈레톤 View를 위해
-					profileVM.isLoading = false
-				} // DispatchQueue
+				profileVM.isLoading = false
 			} // Task
 		} // ScrollView
 	}
