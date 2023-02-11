0f961f68a88d6db31a95a76ed07f4fd22c3e36ed
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-02-11 23:24:16 +0900

    [Feat] 공지사항 UI

--- src/frontend/user/Machacha/Sources/Models/UserNotification.swift
+++ src/frontend/user/Machacha/Sources/Models/UserNotification.swift
@@ -11,7 +11,7 @@ struct UserNotification: Hashable, Identifiable {
 	let id: String
 	let userId: [String]			// 알림을 받을 유저
 	var navigationType: String		// all, home, search, magazine
-	var contentType: String			// 내 근처 알림인지?
+	var title: String				// 공지사항 제목, 나머지 type은 ""
 	var contents: String			// 표출될 내용
 	var updatedAt: Date
 	let createdAt: Date
@@ -42,15 +42,15 @@ struct UserNotification: Hashable, Identifiable {
 	}
 	
 	static func getDummy() -> Self {
-		return UserNotification(id: "0DkzxCKtVprJTu1IacSw", userId: ["egmqxtTT1Zani0UkJpUW"], navigationType: "all", contentType: "전체 알림", contents: "", updatedAt: Date(), createdAt: Date())
+		return UserNotification(id: "0DkzxCKtVprJTu1IacSw", userId: ["egmqxtTT1Zani0UkJpUW"], navigationType: "all", title: "new", contents: "마차차 공지사항 내용입니다", updatedAt: Date(), createdAt: Date())
 	}
 	
 	static func getDummyList() -> [Self] {
 		return [
-			UserNotification(id: "0DkzxCKtVprJTu1IacSw", userId: ["egmqxtTT1Zani0UkJpUW"], navigationType: "all", contentType: "new", contents: "이달의 공지사항", updatedAt: Date(), createdAt: Date()),
-			UserNotification(id: "1DkzxCKtVprJTu1IacSw", userId: ["egmqxtTT1Zani0UkJpUW"], navigationType: "home", contentType: "new", contents: "", updatedAt: Date(), createdAt: Date()),
-			UserNotification(id: "2DkzxCKtVprJTu1IacSw", userId: ["egmqxtTT1Zani0UkJpUW"], navigationType: "search", contentType: "new", contents: "", updatedAt: Date(), createdAt: Date()),
-			UserNotification(id: "3DkzxCKtVprJTu1IacSw", userId: ["egmqxtTT1Zani0UkJpUW"], navigationType: "magazine", contentType: "new", contents: "", updatedAt: Date(), createdAt: Date())
+			UserNotification(id: "0DkzxCKtVprJTu1IacSw", userId: ["egmqxtTT1Zani0UkJpUW"], navigationType: "all", title: "new", contents: "이달의 공지사항", updatedAt: Date(), createdAt: Date()),
+			UserNotification(id: "1DkzxCKtVprJTu1IacSw", userId: ["egmqxtTT1Zani0UkJpUW"], navigationType: "home", title: "new", contents: "", updatedAt: Date(), createdAt: Date()),
+			UserNotification(id: "2DkzxCKtVprJTu1IacSw", userId: ["egmqxtTT1Zani0UkJpUW"], navigationType: "search", title: "new", contents: "", updatedAt: Date(), createdAt: Date()),
+			UserNotification(id: "3DkzxCKtVprJTu1IacSw", userId: ["egmqxtTT1Zani0UkJpUW"], navigationType: "magazine", title: "new", contents: "", updatedAt: Date(), createdAt: Date())
 		]
 	}
 }
