5bf55fd895b071906b960b88ff88be0889e64fbe
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-02-15 19:58:48 +0900

    [Feat] APIConstans 추가

---
+++ src/frontend/admin/MachachaAdmin/Sources/Router(Manager)/APIConstans.swift
@@ -0,0 +1,14 @@
+//
+//  APIConstans.swift
+//  MachachaAdmin
+//
+//  Created by geonhyeong on 2023/02/15.
+//
+
+import Foundation
+
+// 일반적으로 .gitignore로 처리하여, 서버의 주소를 숨김
+struct APIConstants {
+	static let url = "https://fcm.googleapis.com/fcm/send"
+	static let serverKey = "" // lionlike.project.MachachaNoti의 ServerKey가 들어갈 예정
+}
