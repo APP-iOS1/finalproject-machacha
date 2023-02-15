acff48e990f95668098e0dc5ceec46f9ddfb0670
jeoneeee <wldus677@gmail.com>
2023-02-15 15:31:58 +0900

    [Feat] Map Cell 기능 수정

--- src/frontend/user/Machacha/Sources/Views/MapSearch/MapHeader/MapHeaderTagCell.swift
+++ src/frontend/user/Machacha/Sources/Views/MapSearch/MapHeader/MapHeaderTagCell.swift
@@ -73,13 +73,13 @@ struct MapHeaderTagCell: View {
                     mapSearchViewModel.foodCarts = foodCartViewModel.foodCarts
                     Coordinator.shared.foodCarts = mapSearchViewModel.foodCarts
                     Coordinator.shared.setupMarkers()
-//                    Coordinator.shared.mapView
+                    Coordinator.shared.carouselScrolled()
                 } else {
                     Coordinator.shared.removeMarkers()
                     mapSearchViewModel.foodCarts = foodCartViewModel.foodCarts
                     Coordinator.shared.foodCarts = mapSearchViewModel.foodCarts.filter{ $0.bestMenu == bestMenu }
                     Coordinator.shared.setupMarkers()
-
+                    Coordinator.shared.carouselScrolled()
                     
                     mapSearchViewModel.sortedBy(by: bestMenu)
                 }
